e9aa076b3d8b659080ef3d6d0ea8e2307907bda9
Kobbyone4real <metal.acheampong@gmail.com>
2024-11-01 14:02:04 -0400

    Modified show_time.py to display current date, time, and day

--- show_time.py
+++ show_time.py
@@ -1,3 +1,9 @@
 import datetime
 
-print("Current Date and Time:", datetime.datetime.now())
+# Get current date and time
+current_time = datetime.datetime.now()
+
+# Display the current date, time, and day
+print("Current Date and Time:", current_time.strftime("%Y-%m-%d %H:%M:%S"))
+print("Day of the Week:", current_time.strftime("%A"))
+
